244c8dbfcaa328a6760eef92bab8843ce5e67b0e
lucy.poplawska <lucy.poplawska@student.kdg.be>
2026-09-10 17:53:36 +0200

    Setup with the 4 microservices. All run and are visible in Eureka's dashboard

---
+++ db_service/src/test/java/com/cheesouz/db_service/DbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheesouz.db_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
